4a3b100bffa975c6bbb208026ac5be31b70f5f96
Squirrel18 <daniel.quiroga@edunext.co>
2019-06-17 16:33:06 -0500

    Adding integration with Google Big Query.

--- edx2bigquery/load_daily_tracking_logs.py
+++ edx2bigquery/load_daily_tracking_logs.py
@@ -25,6 +25,10 @@ import re
 import datetime
 import bqutil
 import gsutil
+import dateutil.parser
+
+import edx2bigquery_config
+import local_util
 
 #-----------------------------------------------------------------------------
 
@@ -126,13 +130,121 @@ def load_all_daily_logs_for_course(
     sys.stdout.flush()
 
 
-def load_logs_from_local_to_biqquery(course_id):
+def load_local_logs_to_biqquery(course_id, start_date, end_date, verbose):
     """
-    Loads the local tracking logs into Google BigQuery
+    Loads the local tracking logs into Google BigQuery.
+
+    First, it will try to create the dataset if not exist.
 
     Args:
-        course_id:
+        course_id: Course id string.
+        start_date: Start date string to process the tracking logs.
+        end_date: End date string to process the tracking logs.
+        verbose: Whether or not the function logging should be verbose.
     """
-    pass
+    dataset_name = bqutil.course_id2dataset(course_id, dtype="logs")
+    date_pattern = getattr(edx2bigquery_config, 'TRACKING_LOG_REGEX_DATE_PATTERN', '')
+    tracking_start_date, tracking_end_date = get_start_and_end_date(start_date, end_date)
+    schema = get_tracking_log_schema()
+
+    bqutil.create_dataset_if_nonexistent(dataset_name)
+
+    for file_name in local_util.get_tracking_log_file_list(course_id):
+        file_match = re.findall(date_pattern, file_name)
+
+        if not file_match and verbose:
+            logging(
+                'The file name: {} does not have the string date at the end of the name.'.format(
+                    file_name,
+                )
+            )
+            continue
+
+        file_date = dateutil.parser.parse(file_match[-1])
+
+        if file_date <= tracking_end_date and file_date >= tracking_start_date:
+            table_name = 'tracklog_{}'.format(
+                file_date.strftime(getattr(edx2bigquery_config, 'TRACKING_LOG_DATE_FORMAT', '%Y-%m-%d'))
+            )
+
+            if verbose: logging('Uploading: {} to the table: {}'.format(file_name, table_name))
+
+            upload_local_data = bqutil.upload_local_data_to_big_query(
+                dataset_id=dataset_name,
+                table_id=table_name,
+                schema=schema,
+                course_id=course_id,
+                file_name=file_name,
+            )
+        elif verbose:
+            logging(
+                'The file with name: {} has a date before or after of the start_date and end_date provided.'.format(
+                    file_name,
+                )
+            )
+            continue
+
+        if verbose:
+            logging(
+                'The file with name: {} has been succesufully uploaded to Big Query.'.format(
+                    file_name,
+                )
+            )
+
+
+def get_start_and_end_date(start_date, end_date):
+    """
+    Returns the start and end date to process the tracking log.
+
+    If start date was not provided, by default, it will be two days before of the end date.
+    If end date was not provided it will be the day today.
+    If both were not provided, the start date will be two days before since the day today and
+    end day will be the day today.
+
+    Args:
+        start_date: Start date string.
+        end_date: End date string.
+    Returns:
+        tracking_start_date: Final start date to process the tracking logs.
+        tracking_end_date: Final end date to process the tracking logs.
+    """
+    two_days_before = datetime.datetime.now() - datetime.timedelta(days=2)
+
+    if end_date and not start_date:
+        two_days_before = dateutil.parser.parse(end_date) - datetime.timedelta(days=2)
+
+    tracking_start_date = dateutil.parser.parse(start_date) if start_date else two_days_before
+    tracking_end_date = dateutil.parser.parse(end_date) if end_date else datetime.datetime.now()
+
+    return tracking_start_date, tracking_end_date
+
+
+def get_tracking_log_schema():
+    """
+    Returns the tracking schema from the edX2BigQuery schemas folder.
+
+    Returns:
+        Dict containing the tracking log schema.
+    """
+    my_path = os.path.dirname(os.path.realpath(__file__))
+    schema = json.loads(
+        open('{}/schemas/schema_tracking_log.json'.format(my_path)).read()
+    )['tracking_log']
+
+    return schema
+
+
+def logging(message):
+    """
+    Common function to logging the provided message.
+
+    Args:
+        message: The string message to log to.
+    """
+    print('-' * 77)
+    print(message)
+    print('-' * 77)
+    sys.stdout.flush()
+
 
 #-----------------------------------------------------------------------------
