d5426d3baa8b91f606b3d9381e25a0e0837c8349
Squirrel18 <daniel.quiroga@edunext.co>
2019-06-17 16:38:55 -0500

    Revert changes to load_all_daily_logs_for_course function.

--- edx2bigquery/load_daily_tracking_logs.py
+++ edx2bigquery/load_daily_tracking_logs.py
@@ -32,14 +32,8 @@ import local_util
 
 #-----------------------------------------------------------------------------
 
-def load_all_daily_logs_for_course(
-    course_id,
-    use_local_files,
-    gsbucket="gs://x-data",
-    verbose=True,
-    wait=False,
-    check_dates=True,
-):
+def load_all_daily_logs_for_course(course_id, gsbucket="gs://x-data", verbose=True, wait=False,
+                                   check_dates=True):
     '''
     Load daily tracking logs for course from google storage into BigQuery.
     
@@ -50,43 +44,40 @@ def load_all_daily_logs_for_course(
 
     print "Loading daily tracking logs for course %s into BigQuery (start: %s)" % (course_id, datetime.datetime.now())
     sys.stdout.flush()
+    gsroot = gsutil.path_from_course_id(course_id)
 
     mypath = os.path.dirname(os.path.realpath(__file__))
     SCHEMA = json.loads(open('%s/schemas/schema_tracking_log.json' % mypath).read())['tracking_log']
 
-    if not use_local_files:
-        gsroot = gsutil.path_from_course_id(course_id)
-        gsdir = '%s/%s/DAILY/' % (gsbucket, gsroot)
-        file_name_set = gsutil.get_gs_file_list(gsdir)
+    gsdir = '%s/%s/DAILY/' % (gsbucket, gsroot)
 
-    # if use_local_files::
-
+    fnset = gsutil.get_gs_file_list(gsdir)
   
     dataset = bqutil.course_id2dataset(gsroot, dtype="logs")
-    import ipdb; ipdb.set_trace()
   
     # create this dataset if necessary
     bqutil.create_dataset_if_nonexistent(dataset)
+
     tables = bqutil.get_list_of_table_ids(dataset)
     tables = [x for x in tables if x.startswith('track')]
   
     if verbose:
         print "-"*77
         print "current tables loaded:", json.dumps(tables, indent=4)
-        print "files to load: ", json.dumps(file_name_set.keys(), indent=4)
+        print "files to load: ", json.dumps(fnset.keys(), indent=4)
         print "-"*77
         sys.stdout.flush()
   
-    for fn, fninfo in file_name_set.iteritems():
+    for fn, fninfo in fnset.iteritems():
 
         if int(fninfo['size'])<=45:
             print "Zero size file %s, skipping" % fn
             continue
 
         m = re.search('(\d\d\d\d-\d\d-\d\d)', fn)
-        # if not m:
-        #     continue
-        date = '2019-06-13'
+        if not m:
+            continue
+        date = m.group(1)
         tablename = "tracklog_%s" % date.replace('-','')	# YYYYMMDD for compatibility with table wildcards
 
         # file_date = gsutil.get_local_file_mtime_in_utc(fn, make_tz_unaware=True)
@@ -114,14 +105,7 @@ def load_all_daily_logs_for_course(
             print "start [%s]" % datetime.datetime.now()
         sys.stdout.flush()
         gsfn = fninfo['name']
-        import ipdb; ipdb.set_trace()
-        upload_local_data = bqutil.upload_local_data_to_big_query(
-            dataset_id=dataset,
-            table_id=tablename,
-            schema=SCHEMA,
-            course_id=course_id,
-        )
-        # ret = bqutil.load_data_to_table(dataset, tablename, gsfn, SCHEMA, wait=wait, maxbad=1000)
+        ret = bqutil.load_data_to_table(dataset, tablename, gsfn, SCHEMA, wait=wait, maxbad=1000)
   
     if verbose:
         print "-" * 77
