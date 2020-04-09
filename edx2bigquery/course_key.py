917774e1e071ee3ee6167d0e4983e272316bd601
Squirrel18 <daniel.quiroga@edunext.co>
2020-04-08 21:20:42 -0500

    Adding support to CCX course ids. Fixing tracking log schema.

--- edx2bigquery/course_key.py
+++ edx2bigquery/course_key.py
@@ -6,6 +6,8 @@ The converter functions were made in order to mantain the current functionality.
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 
+CCX_CANONICAL_NAMESPACE = 'ccx-v1'
+
 
 def from_deprecated_course_id_string(course_id):
     """
@@ -21,7 +23,7 @@ def from_deprecated_course_id_string(course_id):
     course_key = CourseKey.from_string(course_id)
 
     if course_key.deprecated:
-        return '{}:{}'.format(course_key.CANONICAL_NAMESPACE, course_key._to_string())
+        return str(course_key)
 
     return course_id
 
@@ -39,7 +41,31 @@ def to_deprecated_course_id_string(course_id):
     """
     course_key = CourseKey.from_string(course_id)
 
-    if not course_key.deprecated:
-        return course_key._to_deprecated_string()
+    if course_key.deprecated:
+        return course_id
 
-    return course_id
+    if course_key.CANONICAL_NAMESPACE == CCX_CANONICAL_NAMESPACE:
+        return to_ccx_string(course_key)
+
+    return course_key._to_deprecated_string()
+
+
+def to_ccx_string(course_key):
+    """
+    Returns a forward slash style CCX course id string similar to a deprecated course id style.
+    This function is required because the _to_deprecated_string method in the CCXLocator class
+    is not implemented because the CCX course keys are not supposed to have a deprecated value.
+
+    e.g. ccx-v1:edX+DemoX.1+2014+ccx@10 -> edX/DemoX.1/2014/10
+
+    Args:
+        course_key: opaque_keys.edx.keys.CourseKey instance.
+    Returns:
+        CCX course id string.
+    """
+    return '{org}/{course_number}/{course_run}/{ccx_id}'.format(
+        org=course_key.org,
+        course_number=course_key.course,
+        course_run=course_key.run,
+        ccx_id=course_key.ccx,
+    )
