9d344f05e1cda56b05c5397f51c7d63fdfe14e40
Squirrel18 <daniel.quiroga@edunext.co>
2020-07-16 19:54:37 -0500

    Adding rsa package.

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'pandas==0.24.2',
         'Unidecode==1.1.1',
         'edx-ccx-keys==0.2.2',
+        'rsa<4.1',
     ],
     dependency_links = [],
     package_dir={'edx2bigquery': 'edx2bigquery'},
